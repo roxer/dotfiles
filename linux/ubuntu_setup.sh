3595a0424da2cad3b7281114328069485b5aafbc
Paweł Przyłucki <roxermc@gmail.com>
2021-12-20 23:56:01 +0100

    Update ubuntu_setup.sh

--- linux/ubuntu_setup.sh
+++ linux/ubuntu_setup.sh
@@ -128,7 +128,7 @@ gem i -N tmuxinator
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $(whoami)
 # sudo gpasswd -a deploy docker
 
